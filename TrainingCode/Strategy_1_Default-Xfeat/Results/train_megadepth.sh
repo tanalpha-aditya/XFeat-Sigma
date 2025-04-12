b325d442518753743c460b228b3d98ae99f9b981
tanalpha-aditya <raghuvanshiadityapiloy@gmail.com>
2025-04-12 21:04:44 +0530

    Adding training script and results for Strategy 1

---
+++ TrainingCode/Strategy_1_Default-Xfeat/Results/train_megadepth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=neuro
+#SBATCH --partition=ihub
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50000
+#SBATCH --time=4-00:00:00
+#SBATCH --output=train_megadepth.txt
+#SBATCH --nodelist=gnode093
+
+python3 -m modules.training.train --megadepth_root_path /scratch/narasimha.pai --synthetic_root_path /scratch/narasimha.pai/coco/train2017  --ckpt_save_path ./megadepth_only_checkpoints --training_type xfeat_default --batch_size 16 --n_steps 165000 --lr 3e-4 --device_num 0
