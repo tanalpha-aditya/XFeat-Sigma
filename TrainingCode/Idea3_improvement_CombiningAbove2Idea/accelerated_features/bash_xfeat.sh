d298bf18ee58913696cf8faaebd3153fd4b5b69d
tanalpha-aditya <raghuvanshiadityapiloy@gmail.com>
2025-04-16 02:13:06 +0530

    Adding training files for IDEA 3

---
+++ TrainingCode/Idea3_improvement_CombiningAbove2Idea/accelerated_features/bash_xfeat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=5
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50000
+#SBATCH --time=4-00:00:00
+#SBATCH --nodelist=gnode076
+#SBATCH --output=Stdout_xfeat.txt
+
+python3 -m modules.training.train --megadepth_root_path /scratch/yash9439 --ckpt_save_path ./megadepth_only_checkpoints --training_type xfeat_megadepth --batch_size 16 --n_steps 110000 --lr 3e-4 --device_num 0
